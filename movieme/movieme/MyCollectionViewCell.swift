054d7463c344bd29a9ca4bc74c4ed4a0eb56d2dd
clarissavazquez <cvasquez-ramo@csumb.edu>
2016-12-19 20:40:07 -0800

    Drafted UI Layout.
    Started defining the UI layout for the home, search, and search results page. Embedded the controllers in tab bar/navigation controller. Added a collection view for the home page and search results page. Cells in collection view hold garbage data for now.

---
+++ movieme/movieme/MyCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  MyCollectionViewCell.swift
+//  movieme
+//
+//  Created by Clarissa Vazquez on 12/19/16.
+//  Copyright © 2016 movieme. All rights reserved.
+//
+
+import UIKit
+class MyCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var recLabel: UILabel!
+   
+    @IBOutlet weak var resultLabel: UILabel!
+}
---
+++ movieme/movieme/SearchResultsViewController.swift
@@ -0,0 +1,56 @@
+//
+//  SearchResultsViewController.swift
+//  movieme
+//
+//  Created by Clarissa Vazquez on 12/18/16.
+//  Copyright © 2016 movieme. All rights reserved.
+//
+
+import UIKit
+
+class SearchResultsViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    let reuseIdentifier = "cell" // also enter this string as the cell identifier in the storyboard
+    var items = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48"]
+    
+    
+    // MARK: - UICollectionViewDataSource protocol
+    
+    // Tell collection view how many cells to make
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.items.count
+    }
+    
+    // Make a cell for each cell index path
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        // Get a reference to our storyboard cell
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(reuseIdentifier, forIndexPath: indexPath) as! MyCollectionViewCell
+        
+        // Use the outlet in our custom class to get a reference to the UILabel in the cell
+        cell.resultLabel.text = self.items[indexPath.item]
+        cell.backgroundColor = UIColor.cyanColor()
+        
+        return cell
+    }
+    
+    // MARK: - UICollectionViewDelegate protocol
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: NSIndexPath) {
+        // handle tap events
+        print("You selected cell #\(indexPath.item)!")
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
